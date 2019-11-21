116ff6e94cd5ce314c2dfc8898dafaa3707f6a98
Richard Armstrong <richarms@ska.ac.za>
2019-11-21 15:04:57 +0200

    first commit

---
+++ data/make_dataset.py
@@ -0,0 +1,6 @@
+def prepocess_train_array(data):
+    # a function that reshapes and renormalises the training numpy array
+    im_arr = np.load(data).reshape(23194, 150, 150,1).astype('float32')
+    im_arr = im_arr[:,11:139, 11:139,:]
+    im_arr = (im_arr - 127.5) / 127.5 # Normalize the images to [-1, 1]
+    return im_arr
